32e3fbb3559630b6325e98c9812e343a15e126a8
ryannyunn <ryannyunn@gmail.com>
2017-04-23 22:38:46 -0400

    add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nusselt Number Calculator
+
+## Description
+
+Application for calculating [nusselt numbers](https://en.wikipedia.org/wiki/Nusselt_number).
+
+## Background
+
+This application is currently under development for [Professor George Sidebotham](https://cooper.edu/engineering/people/george-w-sidebotham)'s ME342 Heat Transfer course at The Cooper Union. In collaboration with Robert Faddoul and Cristian Lacey.
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
-// Standard Webpack Configuration
-// 01-17-2016
-
-// webpack module
 const webpack = require('webpack');
-// path module used to resolve absolute paths
 const path = require('path');
-// cleans the bundled directory between builds
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const PATHS = {
@@ -16,14 +10,10 @@ const PATHS = {
 };
 
 module.exports = {
-  // webpack will only compile the main javascript file
-  // entry points to the path of the main js file, here called 'index'
   context: PATHS.app,
   entry: PATHS.main,
   output: {
-    // where the compiled & packed file will be saved
     path: PATHS.output,
-    // what the compiled & packed file will be named
     filename: 'main.js'
   },
 
@@ -39,32 +29,22 @@ module.exports = {
   },
 
   module: {
-    // linter run before packing
-    // test uses regular expressions to match files
     preLoaders: [
       {
-        // lints all javascript
         test: /\.js$/,
-        // excludes node modules from the linting process
         exclude: /node_modules/,
         loader: 'jshint',
       }
     ],
-    // loaders compile and pack different files
-    // test uses regular expressions to match files
     loaders: [
-      // compile all javascript files using the babel-loader module
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel',
-        // preset determine which babel libraries to use
         query: {
           presets: ['es2015', 'react']
         }
       },
-      // add HTML assets to the build folder
-			// used to ensure index.html is carried through the build
       {
         test: /\.html$/,
         loader: 'file',
@@ -72,23 +52,16 @@ module.exports = {
           name: '[name].[ext]'
         }
       },
-      // compile sass files using the sass-loader module
-      // stored in the compiled javascript file
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         loaders: ['style', 'css', 'sass']
       },
-      // compile css files using the css-loader module
-      // stored in the compiled javascript file
       {
         test: /\.css$/,
         exclude: /node_modules/,
         loaders: ['style','css', 'autoprefixer']
       },
-      // compile local images
-      // hash file names to prevent cacheing
-      // copy into 'img' sub-directory
       {
         test: /\.(png|jpg|gif|svg)$/,
         exclude: /node_modules/,
@@ -97,7 +70,6 @@ module.exports = {
           name: 'assets/img/img-[hash:6].[ext]'
 				}
       },
-      // add favicon to build
       {
         test: /\.ico$/,
         exclude: /node_modules/,
@@ -106,9 +78,6 @@ module.exports = {
           name: '[name].[ext]'
         }
       },
-      // compile local font files
-      // hash file names to prevent cacheing
-      // copy into 'font' sub-directory
       {
         test:   /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url',
